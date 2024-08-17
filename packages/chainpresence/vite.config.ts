8dc2f4c9e8381b1c5fe5e96680191342b3056d46
Robin Bryce <robin.bryce@datatrails.ai>
2024-08-17 12:34:52 +0100

    fix: vite config examples for polyfiling web3auth

--- packages/chainpresence/vite.config.ts
+++ packages/chainpresence/vite.config.ts
@@ -7,3 +7,29 @@ export default defineConfig({
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
 });
+
+// https://web3auth.io/docs/troubleshooting/svelte-issues
+// import { sveltekit } from "@sveltejs/kit/vite";
+// import { defineConfig } from "vitest/config";
+// import { nodePolyfills } from "vite-plugin-node-polyfills";
+//
+// export default defineConfig({
+//   plugins: [
+//     nodePolyfills({
+//       exclude: ["fs"],
+//       globals: {
+//         Buffer: true,
+//         global: true,
+//         process: true,
+//       },
+//       protocolImports: true,
+//     }),
+//     sveltekit(),
+//   ],
+//   optimizeDeps: {
+//     include: ["dayjs/plugin/relativeTime.js", "dayjs", "@web3auth/ethereum-provider"],
+//   },
+//   test: {
+//     include: ["src/**/*.{test,spec}.{js,ts}"],
+//   },
+// });
